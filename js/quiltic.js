1dc5e40aeb1471700f47c85b0d5365a7c5a598ff
dmackinnon1 <dmackinnon1@gmail.com>
2017-01-02 15:44:28 -0500

    minor fix

--- js/quiltic.js
+++ js/quiltic.js
@@ -6,7 +6,6 @@ quiltic.sizeFactor = 20;
 quiltic.strokeWidth = 3;
 
 quiltic.tile = function(t,l,b,r) {
-
 	var img = "<svg align='center' width='" + (quiltic.sizeFactor*4) + "' height='" + (quiltic.sizeFactor*4) +"'>";
 	//corners
 	img += quiltic.poly([quiltic.point(0,0), quiltic.point(1,0), quiltic.point(0,1)]);
@@ -15,7 +14,7 @@ quiltic.tile = function(t,l,b,r) {
 	img += quiltic.poly([quiltic.point(4,3), quiltic.point(3,4), quiltic.point(4,4)]);
 	//center
 	img += quiltic.poly([quiltic.point(1,2), quiltic.point(2,1), quiltic.point(3,2), quiltic.point(2,3)]);
-	
+	//open and closed paths
 	if(l) {	
 		img += quiltic.line(quiltic.lpoints(0,1,1,2));
 	} else {
@@ -36,13 +35,11 @@ quiltic.tile = function(t,l,b,r) {
 	} else {
 		img += quiltic.line(quiltic.lpoints(1,4,3,4));
 	}
-
-
-
 	img += "</svg>";
 	return img;
 };
 
+//helpers for tile
 quiltic.point = function(x, y) {
 	return "" + (x*quiltic.sizeFactor) + "," + (y*quiltic.sizeFactor);
 };
@@ -68,9 +65,4 @@ quiltic.line = function(points) {
 	line += " stroke-width='"+ quiltic.strokeWidth + "' stroke='black'/>";
 	return line;	
 };
-
-
-		
-
-
 
